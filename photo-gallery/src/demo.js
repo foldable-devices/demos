de9186e0ac681b124690c7cdbeac27a7f2d83c02
Alexis Menard <alexis.menard@intel.com>
2020-04-21 16:28:25 -0700

    Make the photo-gallery work on Safari.

--- photo-gallery/src/demo.js
+++ photo-gallery/src/demo.js
@@ -1,9 +1,9 @@
-import { LitElement, html, css as litCSS, customElement } from './web_modules/lit-element.js';
-import { classMap } from './web_modules/lit-html/directives/class-map.js';
-import './web_modules/@material/mwc-checkbox.js';
-import './web_modules/@material/mwc-drawer.js';
-import './web_modules/@material/mwc-icon-button.js';
-import { adjustCSS, observe } from "./web_modules/spanning-css-polyfill.js";
+import { LitElement, html, css as litCSS } from '../web_modules/lit-element.js';
+import { classMap } from '../web_modules/lit-html/directives/class-map.js';
+import '../web_modules/@material/mwc-checkbox.js';
+import '../web_modules/@material/mwc-drawer.js';
+import '../web_modules/@material/mwc-icon-button.js';
+import { adjustCSS, observe } from "../web_modules/spanning-css-polyfill.js";
 
 const css = (strings, ...values) => {
   const string = adjustCSS(strings[0], "main-application");
@@ -567,49 +567,49 @@ export class MainApplication extends LitElement {
 
   render() {
     const images = [
-      { name: "images/air-balloon-l", alt: "This is a beautiful picture of an air balloon in the sky." },
-      { name: "images/asia-l", alt: "This photo depicts a woman on a boat somewhere in Asia." },
-      { name: "images/china-l", alt: "Architecture in China, a tower of a building." },
-      { name: "images/church-l", alt: "A black church in the middle of nowhere." },
-      { name: "images/city-l", alt: "A modern city somewhere in Asia." },
-      { name: "images/waterfall2-l", alt: "River with a tiny waterfall." },
-      { name: "images/cloud-l", alt: "Clouds in the sky, view from high altitude." },
-      { name: "images/desert-l", alt: "A desert with cactus." },
-      { name: "images/river2-l", alt: "A river inside a canyon." },
-      { name: "images/disney-l", alt: "The Disney castle in Orlando" },
-      { name: "images/forest-l", alt: "A road crossing a green forest" },
-      { name: "images/greece-l", alt: "Greece architecture" },
-      { name: "images/city2-l", alt: "A city street with an arch" },
-      { name: "images/lake-l", alt: "Women coming out of a lake somewhere lost in the nature" },
-      { name: "images/mountain-l", alt: "Peak of a high mountain and a cloudy sky" },
-      { name: "images/new-york-l", alt: "A street in New York" },
-      { name: "images/pool-l", alt: "Relaxing pool in a luxury hotel" },
-      { name: "images/restaurant-l", alt: "Restaurant on the edge of a river somewhere in France" },
-      { name: "images/river-l", alt: "River with people kayaking" },
-      { name: "images/road-l", alt: "Long straight road somewhere in USA" },
-      { name: "images/sand-l", alt: "Desert with rocky mountains on the background" },
-      { name: "images/sea-l", alt: "Beautiful transparent sea water somewhere in the pacific" },
-      { name: "images/sfo-l", alt: "Golden gate in San Francisco" },
-      { name: "images/stars-l", alt: "Wonderful astronomy shot of stars in the sky" },
-      { name: "images/tents-l", alt: "Camping tents hanging on a cliff" },
-      { name: "images/waterfall-l", alt: "Picture of a waterfall between big rocks" },
-      { name: "images/mountain2-l", alt: "Beautiful picture of a mountain landscape" },
-      { name: "images/wave-l", alt: "This is a picture from a wave in the ocean" },
-      { name: "images/aerial-l", alt: "This is an aerial picture of a city" },
-      { name: "images/building-l", alt: "This is a picture from inside a building" },
-      { name: "images/catamaran-l", alt: "This is a picture of a catamaran with blue water" },
-      { name: "images/cats-l", alt: "Thisa picture of two cats sleeping" },
-      { name: "images/egypt-l", alt: "This is a picture from somewhere in Egypt" },
-      { name: "images/feather-l", alt: "This is a picture of colorful feathers" },
-      { name: "images/fruits-l", alt: "This is a picture of a water bottle and fruits" },
-      { name: "images/golden-gate-l", alt: "This is a picture of the Golden Gate" },
-      { name: "images/marocco-l", alt: "This is a picture of ancient building in Marocco" },
-      { name: "images/milky-way-l", alt: "This is a picture from the Milky Way" },
-      { name: "images/palm-tree-l", alt: "This is a picture of palm trees with beautiful weather" },
-      { name: "images/rainbow-l", alt: "This is a picture of a rainbow from a light" },
-      { name: "images/road2-l", alt: "This is a picture from a road with a lot of trees" },
-      { name: "images/surf-l", alt: "This is a picture of a surfer" },
-      { name: "images/volcano-l", alt: "This is an aerial picture of a volcano" }
+      { name: "../images/air-balloon-l", alt: "This is a beautiful picture of an air balloon in the sky." },
+      { name: "../images/asia-l", alt: "This photo depicts a woman on a boat somewhere in Asia." },
+      { name: "../images/china-l", alt: "Architecture in China, a tower of a building." },
+      { name: "../images/church-l", alt: "A black church in the middle of nowhere." },
+      { name: "../images/city-l", alt: "A modern city somewhere in Asia." },
+      { name: "../images/waterfall2-l", alt: "River with a tiny waterfall." },
+      { name: "../images/cloud-l", alt: "Clouds in the sky, view from high altitude." },
+      { name: "../images/desert-l", alt: "A desert with cactus." },
+      { name: "../images/river2-l", alt: "A river inside a canyon." },
+      { name: "../images/disney-l", alt: "The Disney castle in Orlando" },
+      { name: "../images/forest-l", alt: "A road crossing a green forest" },
+      { name: "../images/greece-l", alt: "Greece architecture" },
+      { name: "../images/city2-l", alt: "A city street with an arch" },
+      { name: "../images/lake-l", alt: "Women coming out of a lake somewhere lost in the nature" },
+      { name: "../images/mountain-l", alt: "Peak of a high mountain and a cloudy sky" },
+      { name: "../images/new-york-l", alt: "A street in New York" },
+      { name: "../images/pool-l", alt: "Relaxing pool in a luxury hotel" },
+      { name: "../images/restaurant-l", alt: "Restaurant on the edge of a river somewhere in France" },
+      { name: "../images/river-l", alt: "River with people kayaking" },
+      { name: "../images/road-l", alt: "Long straight road somewhere in USA" },
+      { name: "../images/sand-l", alt: "Desert with rocky mountains on the background" },
+      { name: "../images/sea-l", alt: "Beautiful transparent sea water somewhere in the pacific" },
+      { name: "../images/sfo-l", alt: "Golden gate in San Francisco" },
+      { name: "../images/stars-l", alt: "Wonderful astronomy shot of stars in the sky" },
+      { name: "../images/tents-l", alt: "Camping tents hanging on a cliff" },
+      { name: "../images/waterfall-l", alt: "Picture of a waterfall between big rocks" },
+      { name: "../images/mountain2-l", alt: "Beautiful picture of a mountain landscape" },
+      { name: "../images/wave-l", alt: "This is a picture from a wave in the ocean" },
+      { name: "../images/aerial-l", alt: "This is an aerial picture of a city" },
+      { name: "../images/building-l", alt: "This is a picture from inside a building" },
+      { name: "../images/catamaran-l", alt: "This is a picture of a catamaran with blue water" },
+      { name: "../images/cats-l", alt: "Thisa picture of two cats sleeping" },
+      { name: "../images/egypt-l", alt: "This is a picture from somewhere in Egypt" },
+      { name: "../images/feather-l", alt: "This is a picture of colorful feathers" },
+      { name: "../images/fruits-l", alt: "This is a picture of a water bottle and fruits" },
+      { name: "../images/golden-gate-l", alt: "This is a picture of the Golden Gate" },
+      { name: "../images/marocco-l", alt: "This is a picture of ancient building in Marocco" },
+      { name: "../images/milky-way-l", alt: "This is a picture from the Milky Way" },
+      { name: "../images/palm-tree-l", alt: "This is a picture of palm trees with beautiful weather" },
+      { name: "../images/rainbow-l", alt: "This is a picture of a rainbow from a light" },
+      { name: "../images/road2-l", alt: "This is a picture from a road with a lot of trees" },
+      { name: "../images/surf-l", alt: "This is a picture of a surfer" },
+      { name: "../images/volcano-l", alt: "This is an aerial picture of a volcano" }
     ];
 
     return html`
@@ -625,7 +625,7 @@ export class MainApplication extends LitElement {
               <figure class="gallery-item">
                 <picture @click="${this._openPicture}">
                   <source srcset="${images.name}.webp" type="image/webp">
-                  <img data-src="${images.name}.jpg" class="gallery-img" alt=${images.alt}>
+                  <img src="${images.name}.jpg" class="gallery-img" alt=${images.alt}>
                 </picture>
               </figure>
             `)}
