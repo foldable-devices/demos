9988f2255fd8709d37d05338572b3ef38de605b8
Alexis Menard <alexis.menard@intel.com>
2020-04-22 12:10:20 -0700

    Unbreak the gallery.

--- photo-gallery/dist/demo.js
+++ photo-gallery/dist/demo.js
@@ -309,133 +309,133 @@ export class MainApplication extends LitElement {
 
   render() {
     const images = [{
-      name: "../images/air-balloon-l",
+      name: "images/air-balloon-l",
       alt: "This is a beautiful picture of an air balloon in the sky."
     }, {
-      name: "../images/asia-l",
+      name: "images/asia-l",
       alt: "This photo depicts a woman on a boat somewhere in Asia."
     }, {
-      name: "../images/china-l",
+      name: "images/china-l",
       alt: "Architecture in China, a tower of a building."
     }, {
-      name: "../images/church-l",
+      name: "images/church-l",
       alt: "A black church in the middle of nowhere."
     }, {
-      name: "../images/city-l",
+      name: "images/city-l",
       alt: "A modern city somewhere in Asia."
     }, {
-      name: "../images/waterfall2-l",
+      name: "images/waterfall2-l",
       alt: "River with a tiny waterfall."
     }, {
-      name: "../images/cloud-l",
+      name: "images/cloud-l",
       alt: "Clouds in the sky, view from high altitude."
     }, {
-      name: "../images/desert-l",
+      name: "images/desert-l",
       alt: "A desert with cactus."
     }, {
-      name: "../images/river2-l",
+      name: "images/river2-l",
       alt: "A river inside a canyon."
     }, {
-      name: "../images/disney-l",
+      name: "images/disney-l",
       alt: "The Disney castle in Orlando"
     }, {
-      name: "../images/forest-l",
+      name: "images/forest-l",
       alt: "A road crossing a green forest"
     }, {
-      name: "../images/greece-l",
+      name: "images/greece-l",
       alt: "Greece architecture"
     }, {
-      name: "../images/city2-l",
+      name: "images/city2-l",
       alt: "A city street with an arch"
     }, {
-      name: "../images/lake-l",
+      name: "images/lake-l",
       alt: "Women coming out of a lake somewhere lost in the nature"
     }, {
-      name: "../images/mountain-l",
+      name: "images/mountain-l",
       alt: "Peak of a high mountain and a cloudy sky"
     }, {
-      name: "../images/new-york-l",
+      name: "images/new-york-l",
       alt: "A street in New York"
     }, {
-      name: "../images/pool-l",
+      name: "images/pool-l",
       alt: "Relaxing pool in a luxury hotel"
     }, {
-      name: "../images/restaurant-l",
+      name: "images/restaurant-l",
       alt: "Restaurant on the edge of a river somewhere in France"
     }, {
-      name: "../images/river-l",
+      name: "images/river-l",
       alt: "River with people kayaking"
     }, {
-      name: "../images/road-l",
+      name: "images/road-l",
       alt: "Long straight road somewhere in USA"
     }, {
-      name: "../images/sand-l",
+      name: "images/sand-l",
       alt: "Desert with rocky mountains on the background"
     }, {
-      name: "../images/sea-l",
+      name: "images/sea-l",
       alt: "Beautiful transparent sea water somewhere in the pacific"
     }, {
-      name: "../images/sfo-l",
+      name: "images/sfo-l",
       alt: "Golden gate in San Francisco"
     }, {
-      name: "../images/stars-l",
+      name: "images/stars-l",
       alt: "Wonderful astronomy shot of stars in the sky"
     }, {
-      name: "../images/tents-l",
+      name: "images/tents-l",
       alt: "Camping tents hanging on a cliff"
     }, {
-      name: "../images/waterfall-l",
+      name: "images/waterfall-l",
       alt: "Picture of a waterfall between big rocks"
     }, {
-      name: "../images/mountain2-l",
+      name: "images/mountain2-l",
       alt: "Beautiful picture of a mountain landscape"
     }, {
-      name: "../images/wave-l",
+      name: "images/wave-l",
       alt: "This is a picture from a wave in the ocean"
     }, {
-      name: "../images/aerial-l",
+      name: "images/aerial-l",
       alt: "This is an aerial picture of a city"
     }, {
-      name: "../images/building-l",
+      name: "images/building-l",
       alt: "This is a picture from inside a building"
     }, {
-      name: "../images/catamaran-l",
+      name: "images/catamaran-l",
       alt: "This is a picture of a catamaran with blue water"
     }, {
-      name: "../images/cats-l",
+      name: "images/cats-l",
       alt: "Thisa picture of two cats sleeping"
     }, {
-      name: "../images/egypt-l",
+      name: "images/egypt-l",
       alt: "This is a picture from somewhere in Egypt"
     }, {
-      name: "../images/feather-l",
+      name: "images/feather-l",
       alt: "This is a picture of colorful feathers"
     }, {
-      name: "../images/fruits-l",
+      name: "images/fruits-l",
       alt: "This is a picture of a water bottle and fruits"
     }, {
-      name: "../images/golden-gate-l",
+      name: "images/golden-gate-l",
       alt: "This is a picture of the Golden Gate"
     }, {
-      name: "../images/marocco-l",
+      name: "images/marocco-l",
       alt: "This is a picture of ancient building in Marocco"
     }, {
-      name: "../images/milky-way-l",
+      name: "images/milky-way-l",
       alt: "This is a picture from the Milky Way"
     }, {
-      name: "../images/palm-tree-l",
+      name: "images/palm-tree-l",
       alt: "This is a picture of palm trees with beautiful weather"
     }, {
-      name: "../images/rainbow-l",
+      name: "images/rainbow-l",
       alt: "This is a picture of a rainbow from a light"
     }, {
-      name: "../images/road2-l",
+      name: "images/road2-l",
       alt: "This is a picture from a road with a lot of trees"
     }, {
-      name: "../images/surf-l",
+      name: "images/surf-l",
       alt: "This is a picture of a surfer"
     }, {
-      name: "../images/volcano-l",
+      name: "images/volcano-l",
       alt: "This is an aerial picture of a volcano"
     }];
     return html`
