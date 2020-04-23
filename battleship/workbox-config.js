41a49f36a476a5e26f6fbced8df37174dbaf9c25
Alexis Menard <alexis.menard@intel.com>
2020-04-23 13:35:14 -0700

    Optimize the workbox prechaching by only sending the minimum set of assets for the demo.
    The rest will be cached along the way in a runtime cache.

--- battleship/workbox-config.js
+++ battleship/workbox-config.js
@@ -2,9 +2,44 @@ module.exports = {
   "globDirectory": ".",
   "inlineWorkboxRuntime": "true",
   "globPatterns": [
-    "**/*.{js,ico,jpg,webp,png,gif,xml,svg,html,css}",
+    "**/*.{js,ico,html,css}",
+    "images/manifest/icon-512x512.png",
+    "images/manifest/icon-192x192.png",
+    "images/manifest/safari-pinned-tab.svg",
     "manifest.json",
+    "images/battleship-top-horizontal.webp",
+    "images/battleship-top-vertical.webp",
+    "images/carrier-top-horizontal.webp",
+    "images/carrier-top-vertical.webp",
+    "images/destroyer-top-horizontal.webp",
+    "images/destroyer-top-vertical.webp",
+    "images/rescue-top-horizontal.webp",
+    "images/rescue-top-vertical.webp",
+    "images/submarine-top-horizontal.webp",
+    "images/submarine-top-vertical.webp",
+    "images/ocean-1024.webp",
+    "images/captain.webp",
+    "images/restart-button.webp",
     "web_modules/import-map.json"
   ],
-  "swDest": "sw.js"
+  "swDest": "sw.js",
+
+  // Define runtime caching rules.
+  runtimeCaching: [{
+    // Match any request that ends with .webp .png, .jpg, .jpeg or .svg.
+    urlPattern: /\.(?:png|jpg|jpeg|svg|webp)$/,
+
+    // Apply a cache-first strategy.
+    handler: 'CacheFirst',
+
+    options: {
+      // Use a custom cache name.
+      cacheName: 'images',
+
+      // Only cache 15 images.
+      expiration: {
+        maxEntries: 15,
+      },
+    },
+  }],
 };
--- photo-gallery/workbox-config.js
+++ photo-gallery/workbox-config.js
@@ -2,9 +2,31 @@ module.exports = {
   "globDirectory": ".",
   "inlineWorkboxRuntime": "true",
   "globPatterns": [
-    "**/*.{js,ico,jpg,webp,png,gif,xml,svg,html,css}",
+    "**/*.{js,ico,html,css}",
+    "images/manifest/icon-512x512.png",
+    "images/manifest/icon-192x192.png",
+    "images/manifest/safari-pinned-tab.svg",
     "manifest.json",
     "web_modules/import-map.json"
   ],
-  "swDest": "sw.js"
+  "swDest": "sw.js",
+
+  // Define runtime caching rules.
+  runtimeCaching: [{
+    // Match any request that ends with .webp .png, .jpg, .jpeg or .svg.
+    urlPattern: /\.(?:png|jpg|jpeg|svg|webp)$/,
+
+    // Apply a cache-first strategy.
+    handler: 'CacheFirst',
+
+    options: {
+      // Use a custom cache name.
+      cacheName: 'images',
+
+      // Only cache 15 images.
+      expiration: {
+        maxEntries: 15,
+      },
+    },
+  }],
 };
