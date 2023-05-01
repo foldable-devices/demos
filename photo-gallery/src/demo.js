43db9b0461b4d628d38ff32479fd917aed5f4b8d
Alexis Menard <alexis.menard@intel.com>
2023-05-01 13:18:31 -0400

    [Photo Gallery] Unbreak the Surface Duo

--- photo-gallery/src/demo.js
+++ photo-gallery/src/demo.js
@@ -491,12 +491,11 @@ export class MainApplication extends LitElement {
     const source_image = e.currentTarget.children[1].currentSrc;
     const path = source_image.replace('-l', '');
     const hasVerticalSegments = window.matchMedia("(vertical-viewport-segments: 2)").matches;
-    console.log('v: ' + hasVerticalSegments)
     const hasHorizontalSegments = window.matchMedia("(horizontal-viewport-segments: 2)").matches;
-    console.log('h: ' + hasHorizontalSegments)
-    if (navigator.devicePosture != undefined && 
+    if ((navigator.devicePosture != undefined &&
         navigator.devicePosture.type === 'folded' &&
-        hasHorizontalSegments) {
+        hasHorizontalSegments) ||
+        (navigator.devicePosture == undefined && (hasVerticalSegments || hasHorizontalSegments))) {
       this._detail_select.style.display = 'none';
       this._detail.style.visibility = 'visible';
       if (this._detail_img.src === path)
